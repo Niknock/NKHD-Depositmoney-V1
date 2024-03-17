bbcbd8f2c68457969d6927a0407bd3dfcc6f33c8
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-03-17 12:01:36 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,16 +4,22 @@ Config.Markers = {
     {
         Coords = vector3(433.7351, -979.8600, 30.7110), -- Change to your Location
         Radius = 1.5,
+        Type = 21, -- Typ des Markers (1 = Zylinder)
+        Scale = vector3(0.5, 0.5, 0.5), -- Skalierung des Markers
+        Color = {r = 35, g = 178, b = 207, a = 150}, -- Farbe des Markers (RGBA)
         SocietyName = 'police',
         MenuTitle = 'The deposit amount for police', -- Change to your Language
-        PromptText = '~w~Press ~b~E~w~ to open ' -- Change to your Language
+        PromptText = '~w~Press ~INPUT_CONTEXT~ to open the police menu' -- Change to your Language
     },
     {
         Coords = vector3(297.2287, -587.7916, 43.2608), -- Change to your Location
         Radius = 1.5,
+        Type = 21, -- Typ des Markers (1 = Zylinder)
+        Scale = vector3(0.5, 0.5, 0.5), -- Skalierung des Markers
+        Color = {r = 215, g = 29, b = 29, a = 150}, -- Farbe des Markers (RGBA)
         SocietyName = 'ambulance',
         MenuTitle = 'The deposit amount for the ambulance', -- Change to your Language
-        PromptText = '~w~Press ~b~E~w~ to open ' -- Change to your Language
+        PromptText = '~w~Press ~INPUT_CONTEXT~ to open ambulance menu' -- Change to your Language
     }
     --{
     --    Coords = vector3( here your coords ),
@@ -24,6 +30,6 @@ Config.Markers = {
     --},
 }
 
-Config.NotificationInvalidAmount = 'Invalid amount' -- Change to your Language
-Config.NotificationDepositSuccess = 'Deposit successful' -- Change to your Language
+Config.NotificationInvalidAmount = '~r~Invalid amount' -- Change to your Language
+Config.NotificationDepositSuccess = '~g~You have Succesfully Deposited ' -- Change to your Language
 Config.DepositMoneyEvent = 'nkhd:depositMoney'
