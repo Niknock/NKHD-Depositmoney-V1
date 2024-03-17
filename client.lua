9a582e2a6cb41a6c17b492fea48f5452531ec5db
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-03-17 12:01:46 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -7,7 +7,8 @@ Citizen.CreateThread(function()
             local distance = #(pedCoords - marker.Coords)
 
             if distance <= marker.Radius then
-                DrawText3D(marker.Coords.x, marker.Coords.y, marker.Coords.z, marker.PromptText .. marker.MenuTitle)
+                ESX.ShowHelpNotification(marker.PromptText, false, true)
+                DrawMarker(marker.Type, marker.Coords.x, marker.Coords.y, marker.Coords.z - 0.0, 0, 0, 0, 0, 0, 0, marker.Scale.x, marker.Scale.y, marker.Scale.z, marker.Color.r, marker.Color.g, marker.Color.b, marker.Color.a, false, true, 2, nil, nil, false)
                 if IsControlJustPressed(0, 38) then
                     ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'deposit_amount_' .. i, {
                         title = marker.MenuTitle
@@ -15,11 +16,11 @@ Citizen.CreateThread(function()
                         local amount = tonumber(data.value)
                         
                         if amount == nil or amount <= 0 then
-                            ESX.ShowNotification(Config.NotificationInvalidAmount)
+                            ShowNotification(Config.NotificationInvalidAmount)
                         else
                             menu.close()
                             TriggerServerEvent(Config.DepositMoneyEvent, marker.SocietyName, amount)
-                            ESX.ShowNotification(Config.NotificationDepositSuccess)
+                            ShowNotification(Config.NotificationDepositSuccess .. amount .. '$')
                         end
                     end, function(data, menu)
                         menu.close()
@@ -30,22 +31,8 @@ Citizen.CreateThread(function()
     end
 end)
 
-function DrawText3D(x, y, z, text)
-    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-    local p = GetGameplayCamCoords()
-    local distance = GetDistanceBetweenCoords(p.x, p.y, p.z, x, y, z, 1)
-    local scale = (1 / distance) * 2
-    local fov = (1 / GetGameplayCamFov()) * 100
-    scale = scale * fov
-
-    if onScreen then
-        SetTextScale(0.35, 0.35)
-        SetTextFont(4)
-        SetTextProportional(1)
-        SetTextColour(255, 255, 255, 215)
-        SetTextEntry("STRING")
-        SetTextCentre(1)
-        AddTextComponentString(text)
-        DrawText(_x,_y)
-    end
+function ShowNotification(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, false)
 end
