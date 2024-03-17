cae54a6503c44c8a0b7e9d02962dab2c0b77ce10
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-03-17 11:35:02 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,14 +2,14 @@ Config = {}
 
 Config.Markers = {
     {
-        Coords = vector3(2929.8491, 3847.8774, 52.3903),
+        Coords = vector3(433.7351, -979.8600, 30.7110), -- Change to your Location
         Radius = 1.5,
         SocietyName = 'police',
         MenuTitle = 'The deposit amount for police', -- Change to your Language
         PromptText = '~w~Press ~b~E~w~ to open ' -- Change to your Language
     },
     {
-        Coords = vector3(2929.2285, 3844.9155, 52.4042),
+        Coords = vector3(297.2287, -587.7916, 43.2608), -- Change to your Location
         Radius = 1.5,
         SocietyName = 'ambulance',
         MenuTitle = 'The deposit amount for the ambulance', -- Change to your Language
